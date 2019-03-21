76e37ca54453c76c7cb2f8a48a1631d26d30fba2
itai <zeitak.itai@gmail.com>
2019-03-21 23:58:01 +0200

    adding gt

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # --profiler
+# c_sharp_code in https://1drv.ms/f/s!ArDCKXYlrsk8giREYfXOURwFUjLM
 python3.6 main.py --matix_form 0-1 --files_limit 1000 --max_features 1000 --top_similar_functions 100
