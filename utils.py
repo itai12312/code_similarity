f4ecf56b77ad6cada8d9dfd5315e152f189cc6d1
AdarWeidman <adar.weidman@gmail.com>
2019-03-25 09:58:18 +0200

    Fix a small bug

--- utils.py
+++ utils.py
@@ -190,6 +190,8 @@ def fix_blocks_and_methods(input_df):
     # Create a column with the lines of the PREVIOUS token    
     df[4] = df[2].shift(1)
     df[4] = np.where(np.isnan(df[4]),df[4][df.shape[0]-2],df[4]) # last token is not set, so fix it
+    df[4] = np.where(np.isnan(df[4]),df[4][df.shape[0]-3],df[4]) # last token is not set, so fix it
+    df[4] = np.where(np.isnan(df[4]),df[4][df.shape[0]-4],df[4]) # last token is not set, so fix it
 
     # Copy the line number to the END_BLOCK and END_METHOD
     df[2] = np.where(df[0]=="END_BLOCK",np.where(np.isnan(df[2]),df[4],df[2]),df[2])
@@ -198,6 +200,8 @@ def fix_blocks_and_methods(input_df):
     # Create a column with the lines of the NEXT token
     df[4] = df[2].shift(-1)
     df[4] = np.where(np.isnan(df[4]),df[4][0],df[4]) # Fix "still NaN" values
+    df[4] = np.where(np.isnan(df[4]),df[4][1],df[4]) # Fix "still NaN" values
+    df[4] = np.where(np.isnan(df[4]),df[4][2],df[4]) # Fix "still NaN" values
 
     # Copy the line number for BEGIN_BLOCK and BEGIN_METHOD
     df[2] = np.where(df[0]=="BEGIN_BLOCK",np.where(np.isnan(df[2]),df[4],df[2]),df[2])
