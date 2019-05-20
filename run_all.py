48fde13457f254432c45d19eb87e774842c09eea
AdarWeidman <adar.weidman@gmail.com>
2019-05-20 21:32:53 +0300

    Small updates

--- run_all.py
+++ run_all.py
@@ -5,8 +5,9 @@ Created on Mon May 20 11:55:26 2019
 @author: Adar
 """
 
-from main import main
+from analyze_methods import create_vectors
 from vocabulary import create_vocabulary, clean_vocabulary, cut_vocabulary
+import pickle
 
 path = "D:\\Y-Data\\Proj\\tokenized1"
 
@@ -16,8 +17,16 @@ sorted_freq_list = create_vocabulary(path = "D:\\Y-Data\\Proj\\tokenized1", perc
 clean_freq_list = clean_vocabulary(sorted_freq_list) # ideally this one should use a file and not the return value
 freq_list = cut_vocabulary(clean_freq_list) # ideally this one should use a file and not the return value
 
+freq_list = pickle.load(open("short_sorted_freq_list.pkl", "rb"))
+
+
+
+create_vectors(input_folder = path)
+
+"""
 create_vectors_for_methods() #create a vectors file including file name, function name, full vector, list of vlunerabilities in matrix
 normalize_vectors() # into a new file
 create_distance_matrix() #into a new file
 cluster_methods(minimal_cluster = 20) # into a set of files - each file for cluster (do not create file for clusters of less than 20 methods)
 
+"""
