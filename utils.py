705ecd7728251b0ef3851106daefd83b94552ea4
itai <zeitak.itai@gmail.com>
2019-05-25 01:34:36 +0300

    fixing bugs

--- utils.py
+++ utils.py
@@ -90,19 +90,24 @@ def create_functions_list_from_filename(item):
     real_curs = []
     for idx in range(len(enders.index)):
         cur = enders.index[idx]
-        realidx = list(df.index).index(cur)
-        temp = df.values[realidx, 2]
-        lim = len(df.values) -1
-        steps = 0
-        steps_num = 7
-        while is_not_ok(temp) and realidx < lim and steps < steps_num:
-            realidx += 1
+        if cur in list(df.index):
+            realidx = list(df.index).index(cur)
             temp = df.values[realidx, 2]
-            steps += 1
-        if (realidx == lim) or steps == steps_num:
-            real_curs.append(-1)
+            lim = len(df.values) -1
+            steps = 0
+            steps_num = 7
+            while is_not_ok(temp) and realidx < lim and steps < steps_num:
+                realidx += 1
+                temp = df.values[realidx, 2]
+                steps += 1
+            if (realidx == lim) or steps == steps_num:
+                real_curs.append(-1)
+                realidx = -1
+            else:
+                real_curs.append(df.index[realidx])
         else:
-            real_curs.append(df.index[realidx])
+            realidx = -1
+            real_curs.append(-1)
         curs.append(df.index[realidx])
     raw_end = df.loc[curs]
     assert len(raw_end) == len(raw_start)
